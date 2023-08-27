df45b2df6a6957aef3833ac842f460a3eeb0b98f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 00:56:31 -0600

    update

--- assets/api-scripts/logout.js
+++ assets/api-scripts/logout.js
@@ -1,27 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('logout-link').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent the default behavior of following the link
+        event.preventDefault(); // Prevent the default behavior of following the link
 
-    // Make a POST request to the logout endpoint
-    fetch('https://api.morganserver.com/api/logout', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        // Include any necessary data in the request body
-        body: JSON.stringify({ token: 'jhduHDJhfF94J9mdjaadf89dfajLJ' }),
-    })
-    .then((response) => {
-        if (response.ok) {
-            // Handle successful logout, e.g., redirect to a login page
-            window.location.href = 'https://app-aarc.morganserver.com/';
-        } else {
-            // Handle logout error, e.g., display an error message
-            console.error('Logout request failed');
-        }
-    })
-    .catch((error) => {
-        console.error('An error occurred during logout', error);
+        // Make a POST request to the logout endpoint
+        fetch('https://api.morganserver.com/api/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((response) => {
+            if (response.ok) {
+                // Handle successful logout, e.g., redirect to a login page
+                window.location.href = 'https://app-aarc.morganserver.com/';
+            } else {
+                // Handle logout error, e.g., display an error message
+                console.error('Logout request failed');
+            }
+        })
+        .catch((error) => {
+            console.error('An error occurred during logout', error);
+        });
     });
-});
 });
