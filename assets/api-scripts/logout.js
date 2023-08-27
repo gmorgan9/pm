46480e0c81aeb24863ed16a86705788ad0d1ee40
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 02:26:58 -0600

    update

--- assets/api-scripts/logout.js
+++ assets/api-scripts/logout.js
@@ -1,27 +1,40 @@
-// document.getElementById('logout-link').addEventListener('click', function (event) {
-//     event.preventDefault(); // Prevent the default behavior of following the link
+document.getElementById('logout-link').addEventListener('click', function (e) {
+    e.preventDefault();
 
-//     // Make a POST request to the logout endpoint
-//     fetch('https://api.morganserver.com/api/logout', {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-// })
-// .then((response) => {
-//     if (response.ok) {
-//         // Handle successful logout, e.g., redirect to a login page
-//         window.location.href = 'https://app-aarc.morganserver.com/';
-//     } else {
-//         // Handle logout error, e.g., display an error message
-//         console.error('Logout request failed - client');
-//         // You can also log the response status and response text for more details
-//         console.log('Response Status:', response.status);
-//         response.text().then((text) => console.log('Response Text:', text));
-//     }
-// })
-// .catch((error) => {
-//     console.error('An error occurred during logout', error);
-// });
+    // Assuming you have some way to identify the logged-in user
+    // In this example, let's assume you have a user_id variable
+    const user_id = getUserId(); // You need to implement getUserId() to get the user's ID
 
-// });
+    // Send a POST request to the logout endpoint
+    fetch('/api/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded', // Set the content type
+        },
+        body: `user_id=${user_id}`, // Send the user_id in the request body
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message === 'Logout successful') {
+            // Redirect the user to the login page or perform any other desired action
+            window.location.href = '/login'; // Change to your login page URL
+        } else {
+            // Handle logout failure
+            alert('Logout failed. Please try again.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while logging out.');
+    });
+});
+
+// Function to get the user's ID (you need to implement this)
+function getUserId() {
+    // Implement a way to retrieve the user's ID here
+    // This could be from a session, JWT, or any other method
+    // For example, if you have a session-based authentication:
+    // return sessionStorage.getItem('user_id');
+    // or if you're using JWT tokens:
+    // return decodeJwtToken().user_id;
+}
