6dd916c2f7a14227b25b3e0ce2d89101e63fa707
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 23:21:14 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -1,55 +1,27 @@
-// Attach an event listener to the form with the ID 'login-form'
-document.getElementById('login-form').addEventListener('submit', handleLogin);
-
-async function handleLogin(event) {
-  event.preventDefault();
-
-  // Capture form data
-  const form = event.target;
-  const formData = new FormData(form);
-  
-  // Extract the values of 'work_email' and 'password' fields from the form data
-  const work_email = formData.get('work_email');
-  const password = formData.get('password');
-
-  try {
-    // Create an object with the login data
-    const loginData = { work_email, password };
-
-    // Send a POST request to the login endpoint
-    console.log('Sending POST request...');
-
-    // Inside the response handling section of your client-side code
-    const response = await fetch('https://api.morganserver.com/api/login', {
+// Inside the response handling section of your client-side code
+const response = await fetch('https://api.morganserver.com/api/login', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
     },
     body: JSON.stringify(loginData),
-    credentials: 'include', // Include credentials in the request
-    });
-  
+});
 
-    // Check if the response status indicates success (status codes 200-299)
-    if (response.ok) {
-      // Parse the JSON response data
-      const data = await response.json();
+console.log('Server Response:', response);
 
-      if (data.success) {
-        // Redirect to a new page or update the UI for a successful login
-        // console.log('Login successful');
-        window.location.href = 'https://app-aarc.morganserver.com/dashboard';
-      } else {
-        // Display an error message to the user.
-        const errorMessage = document.getElementById('error-message');
-        errorMessage.textContent = data.message;
-      }
-    } else {
-      // Handle non-successful response (e.g., server error)
-      console.error('Login request failed');
-    }
-  } catch (error) {
-    // Handle JavaScript or network errors
-    console.error('An error occurred during login', error);
+if (response.ok) {
+  // Parse the JSON response data
+  const data = await response.json();
+
+  if (data.success) {
+    // Redirect to the URL provided in the response
+    window.location.href = data.redirect;
+  } else {
+    // Display an error message to the user.
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = data.message;
   }
+} else {
+  // Handle non-successful response (e.g., server error)
+  console.error('Login request failed');
 }
