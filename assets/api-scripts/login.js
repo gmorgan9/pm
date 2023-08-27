b8ebe06bfd927840ac659682b0ec217b4f944241
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 23:48:16 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -1,56 +1,39 @@
-// Attach an event listener to the form with the ID 'login-form'
 document.getElementById('login-form').addEventListener('submit', handleLogin);
 
 async function handleLogin(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Capture form data
-  const form = event.target;
-  const formData = new FormData(form);
-  
-  // Extract the values of 'work_email' and 'password' fields from the form data
-  const work_email = formData.get('work_email');
-  const password = formData.get('password');
+    const form = event.target;
+    const formData = new FormData(form);
+    const work_email = formData.get('work_email');
+    const password = formData.get('password');
 
-  try {
-    // Create an object with the login data
-    const loginData = { work_email, password };
+    try {
+        const loginData = { work_email, password };
 
-    // Send a POST request to the login endpoint
-    console.log('Sending POST request...');
+        const response = await fetch('https://api.morganserver.com/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginData),
+        });
 
-    // Inside the response handling section of your client-side code
-    const response = await fetch('https://api.morganserver.com/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(loginData),
-    });
-  
-    console.log('Server Response:', response);
-  
+        if (response.ok) {
+            const data = await response.json();
 
-    // Check if the response status indicates success (status codes 200-299)
-    if (response.ok) {
-      // Parse the JSON response data
-      const data = await response.json();
-
-      if (data.success) {
-        console.log('Login successful, redirecting...');
-        // Redirect to the dashboard upon successful login
-        window.location.href = 'https://app-aarc.morganserver.com/dashboard/';
-      } else {
-        // Display an error message to the user.
-        const errorMessage = document.getElementById('error-message');
-        errorMessage.textContent = data.message;
-      }
-    } else {
-      // Handle non-successful response (e.g., server error)
-      console.error('Login request failed');
+            if (data.success) {
+                // Redirect to the dashboard page upon successful login
+                window.location.href = 'https://app-aarc.morganserver.com/dashboard/';
+            } else {
+                // Display an error message to the user.
+                const errorMessage = document.getElementById('error-message');
+                errorMessage.textContent = data.message;
+            }
+        } else {
+            console.error('Login request failed');
+        }
+    } catch (error) {
+        console.error('An error occurred during login', error);
     }
-  } catch (error) {
-    // Handle JavaScript or network errors
-    console.error('An error occurred during login', error);
-  }
 }
