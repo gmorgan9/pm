06a0c6adc0bcf644d6206af4e23cc72ec500c735
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 22:20:51 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -2,14 +2,11 @@
 document.getElementById('login-form').addEventListener('submit', handleLogin);
 
 async function handleLogin(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formData = new FormData(event.target); // Capture form data
-    console.log(formData.get('work_email'));
-
-  // Retrieve the values of the 'work_email' and 'password' fields from the form
-//   const work_email = document.getElementById('work_email').value;
-//   const password = document.getElementById('password').value;
+  const formData = new FormData(event.target); // Capture form data
+  const work_email = formData.get('work_email'); // Get the work_email from the form data
+  const password = formData.get('password'); // Get the password from the form data
 
   try {
     // Send a POST request to the login endpoint
