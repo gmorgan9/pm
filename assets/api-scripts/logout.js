901ec4f7caa4882ee88b99bde7e76819e74425ac
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 01:14:47 -0600

    update

--- assets/api-scripts/logout.js
+++ assets/api-scripts/logout.js
@@ -1,26 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('logout-link').addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the default behavior of following the link
+document.getElementById('logout-link').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent the default behavior of following the link
 
-        // Make a POST request to the logout endpoint
-        fetch('https://api.morganserver.com/api/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            // You can include any necessary data in the request body here
-        })
-        .then((response) => {
-            if (response.ok) {
-                // Handle successful logout, e.g., redirect to a login page
-                window.location.href = 'https://app-aarc.morganserver.com/';
-            } else {
-                // Handle logout error, e.g., display an error message
-                console.error('Logout request failed - client');
-            }
-        })
-        .catch((error) => {
-            console.error('An error occurred during logout', error);
-        });
+    // Make a POST request to the logout endpoint
+    fetch('https://api.morganserver.com/api/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Include any necessary data in the request body
+        // body: JSON.stringify({ token: 'your-jwt-token' }),
+    })
+    .then((response) => {
+        if (response.ok) {
+            // Handle successful logout, e.g., redirect to a login page
+            window.location.href = 'https://app-aarc.morganserver.com/';
+        } else {
+            // Handle logout error, e.g., display an error message
+            console.error('Logout request failed - client');
+        }
+    })
+    .catch((error) => {
+        console.error('An error occurred during logout', error);
     });
 });
