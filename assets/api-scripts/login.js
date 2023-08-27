381360e6dc86fd0abe011b8ef0665e84f0f68f8a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 20:47:50 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -1,32 +1,46 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
+// Function to handle form submission
+async function handleSubmit(event) {
     event.preventDefault();
+    
+    // Get form input values
+    const work_email = document.getElementById('work_email').value;
+    const password = document.getElementById('password').value;
 
-    const work_email = document.getElementById("work_email").value;
-    const password = document.getElementById("password").value;
+    // Create an object to send as JSON data
+    const data = {
+        work_email,
+        password
+    };
 
-    fetch("https://app-aarc-api.morganserver.com/api/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            work_email: work_email,
-            password: password
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Handle the response from the Flask API
-        if (data.status === "success") {
-            // Redirect to a success page or perform other actions
-            window.location.href = "https://app-aarc.morganserver.com/dashboard/";
+    try {
+        const response = await fetch('https://app-aarc-api.morganserver.com/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            // Assuming the token is returned in the response
+            const token = result.token;
+
+            // You can store the token in localStorage or a cookie for later use
+            localStorage.setItem('token', token);
+
+            // Redirect or perform other actions as needed
+            window.location.href = '/dashboard'; // Redirect to the dashboard
         } else {
-            // Display an error message to the user
-            const errorMessage = document.getElementById("error-message");
-            errorMessage.innerHTML = data.message;
+            const errorData = await response.json();
+            // Display the error message to the user
+            document.getElementById('error-message').textContent = errorData.message;
         }
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
-});
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Add an event listener to the form
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', handleSubmit);
