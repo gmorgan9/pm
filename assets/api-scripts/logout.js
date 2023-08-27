db66f37e38f27a65c65cfbeabb877d3926679728
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 02:32:50 -0600

    update

--- assets/api-scripts/logout.js
+++ assets/api-scripts/logout.js
@@ -5,28 +5,34 @@ document.getElementById('logout-link').addEventListener('click', function (e) {
     // In this example, let's assume you have a user_id variable
     const user_id = getUserId(); // You need to implement getUserId() to get the user's ID
 
-    // Send a POST request to the logout endpoint
-    fetch('https://app-aarc-api.morganserver.com/api/logout', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded', // Set the content type
-        },
-        body: `user_id=${user_id}`, // Send the user_id in the request body
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.message === 'Logout successful') {
-            // Redirect the user to the login page or perform any other desired action
-            window.location.href = 'https://app-aarc.morganserver.com/'; // Change to your login page URL
-        } else {
-            // Handle logout failure
-            alert('Logout failed. Please try again.');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred while logging out.');
-    });
+    // Check if user_id is defined and is a valid integer
+    if (user_id && Number.isInteger(user_id)) {
+        // Send a POST request to the logout endpoint
+        fetch('https://app-aarc-api.morganserver.com/api/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded', // Set the content type
+            },
+            body: `user_id=${user_id}`, // Send the user_id in the request body
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.message === 'Logout successful') {
+                // Redirect the user to the login page or perform any other desired action
+                window.location.href = '/login'; // Change to your login page URL
+            } else {
+                // Handle logout failure
+                alert('Logout failed. Please try again.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred while logging out.');
+        });
+    } else {
+        // Handle the case where user_id is not valid
+        alert('Invalid user ID. Please log in again.');
+    }
 });
 
 // Function to get the user's ID (you need to implement this)
