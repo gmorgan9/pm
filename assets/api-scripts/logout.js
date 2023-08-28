e18a26047e04d153d7193495b45769bbd57b3350
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 17:30:23 -0600

    update

--- assets/api-scripts/logout.js
+++ assets/api-scripts/logout.js
@@ -20,3 +20,15 @@ logoutButton.addEventListener('click', async () => {
         console.error('An error occurred:', error);
     }
 });
+
+// Function to delete a cookie
+function deleteCookie(name) {
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;`;
+}
+
+// Function to retrieve a specific cookie by name
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
