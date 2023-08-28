1a3b03a2e37738200a5e4b89eb82075427047846
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 17:29:49 -0600

    update

--- assets/api-scripts/logout.js
+++ assets/api-scripts/logout.js
@@ -1,47 +1,22 @@
-document.getElementById('logout-link').addEventListener('click', function (e) {
-    e.preventDefault();
+const logoutButton = document.getElementById('logout-link');
 
-    // Assuming you have some way to identify the logged-in user
-    // In this example, let's assume you have a user_id variable
-    const user_id = getUserId(); // You need to implement getUserId() to get the user's ID
-
-    // Check if user_id is defined and is a valid integer
-    if (user_id && Number.isInteger(user_id)) {
-        // Send a POST request to the logout endpoint
-        fetch('https://app-aarc-api.morganserver.com/api/logout', {
+logoutButton.addEventListener('click', async () => {
+    try {
+        const response = await fetch('https://app-aarc-api.morganserver.com/logout', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded', // Set the content type
-                'Origin': 'https://app-aarc.morganserver.com',
-            },
-            body: `user_id=${user_id}`, // Send the user_id in the request body
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message === 'Logout successful') {
-                // Redirect the user to the login page or perform any other desired action
-                window.location.href = '/login'; // Change to your login page URL
-            } else {
-                // Handle logout failure
-                alert('Logout failed. Please try again.');
+                'Authorization': `Bearer ${getCookie('access_token')}`
             }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('An error occurred while logging out.');
         });
-    } else {
-        // Handle the case where user_id is not valid
-        alert('Invalid user ID. Please log in again.');
+
+        if (response.ok) {
+            // Remove the access token cookie
+            deleteCookie('access_token');
+
+        } else {
+            console.error('Logout failed');
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
     }
 });
-
-// Function to get the user's ID (you need to implement this)
-function getUserId() {
-    // Implement a way to retrieve the user's ID here
-    // This could be from a session, JWT, or any other method
-    // For example, if you have a session-based authentication:
-    // return sessionStorage.getItem('user_id');
-    // or if you're using JWT tokens:
-    // return decodeJwtToken().user_id;
-}
