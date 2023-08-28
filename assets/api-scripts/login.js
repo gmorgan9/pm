5aaa020536421d1817128fa2731ab93e05652d9f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 15:33:51 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -25,7 +25,7 @@ loginForm.addEventListener('submit', async (event) => {
             // Instead of storing the token in localStorage, set it as an HTTP-only cookie
             setCookie('access_token', data.access_token, 7); // Replace '7' with your desired cookie expiration in days
         
-            window.location.href = "http://app-aarc.morganserver.com.com/dashboard";
+            window.location.href = "http://app-aarc.morganserver.com/dashboard";
             // loginForm.style.display = 'none';
             // logoutButton.style.display = 'block';
             getUserDetails();
