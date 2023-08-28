4befafdbf9a5c8371a36c55101378f3de5feb454
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 15:18:40 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -20,15 +20,18 @@ loginForm.addEventListener('submit', async (event) => {
         if (response.ok) {
             const data = await response.json();
             
+            console.log('Access Token:', data.access_token); // Log the access token to the console
+        
             // Instead of storing the token in localStorage, set it as an HTTP-only cookie
             setCookie('access_token', data.access_token, 7); // Replace '7' with your desired cookie expiration in days
-
+        
             loginForm.style.display = 'none';
             logoutButton.style.display = 'block';
             getUserDetails();
         } else {
             console.error('Login failed');
         }
+        
     } catch (error) {
         console.error('An error occurred:', error);
     }
