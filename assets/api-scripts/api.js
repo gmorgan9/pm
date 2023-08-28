d77fe37a2a9247a5d218067877860912eb9cfe01
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 00:21:20 -0600

    udp

--- assets/api-scripts/api.js
+++ assets/api-scripts/api.js
@@ -1,23 +1,22 @@
-// // Function to check if a user is logged in and retrieve session data
-// function checkLoginStatus() {
-//     fetch('https://app-aarc-api.morganserver.com/api/check_login')
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.loggedIn) {
-//                 // User is logged in, retrieve additional session data if needed
-//                 fetch('https://app-aarc-api.morganserver.com/api/profile')
-//                     .then(response => response.text())
-//                     .then(profileText => {
-//                         // Display the user's profile information on the frontend
-//                         document.getElementById('profile').textContent = profileText;
-//                     });
-//             } else {
-//                 // User is not logged in, display a message or redirect to the login page
-//                 document.getElementById('profile').textContent = "You are not logged in.";
-//             }
-//         });
-// }
+function checkLoginStatus() {
+    fetch('https://app-aarc-api.morganserver.com/api/check_login')
+        .then(response => response.json())
+        .then(data => {
+            if (data.loggedIn) {
+                // User is logged in, retrieve additional session data if needed
+                fetch('https://app-aarc-api.morganserver.com/api/profile')
+                    .then(response => response.text())
+                    .then(profileText => {
+                        // Display the user's profile information on the frontend
+                        document.getElementById('profile').textContent = profileText;
+                    });
+            } else {
+                // User is not logged in, display a message or redirect to the login page
+                document.getElementById('profile').textContent = "You are not logged in.";
+            }
+        });
+}
 
-// // Call the checkLoginStatus function when your page loads or when needed
-// checkLoginStatus();
+// Call the checkLoginStatus function when your page loads or when needed
+checkLoginStatus();
 
