4237f9c4c2319560ccc969f25668ff9896bde729
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 15:39:56 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -20,7 +20,6 @@ loginForm.addEventListener('submit', async (event) => {
         if (response.ok) {
             const data = await response.json();
             
-            console.log('Data from server:', data); // Log the entire data object to the console
         
             // Instead of storing the token in localStorage, set it as an HTTP-only cookie
             setCookie('access_token', data.access_token, 7); // Replace '7' with your desired cookie expiration in days
