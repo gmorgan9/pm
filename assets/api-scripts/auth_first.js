1336d784d6678aeca21af4de4924eb198cfcee64
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 18:38:59 -0600

    update

--- assets/api-scripts/auth_first.js
+++ assets/api-scripts/auth_first.js
@@ -1,14 +1,16 @@
-// Check if an access token exists
-const accessToken = getCookie('access_token');
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if an access token exists
+    const accessToken = getCookie('access_token');
 
-if (!accessToken) {
-    // Redirect the user to the login page
-    window.location.href = 'https://app-aarc.morganserver.com/';
-}
+    if (!accessToken) {
+        // Redirect the user to the login page
+        window.location.href = 'https://app-aarc.morganserver.com/';
+    }
 
-// Function to retrieve a specific cookie by name
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
+    // Function to retrieve a specific cookie by name
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+});
