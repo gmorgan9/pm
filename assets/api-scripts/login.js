9e65144cb25144798222cf6102b55a6cba3a54e9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 20:56:02 -0600

    update

--- assets/api-scripts/login.js
+++ assets/api-scripts/login.js
@@ -25,7 +25,7 @@ loginForm.addEventListener('submit', async (event) => {
         } else {
             // Display the error message
             // const errordata = await response.json();
-            errorMessageContainer.innerHTML = '<div class="alert alert-danger" role="alert">Login failed. Please check your credentials.</div>';
+            errorMessageContainer.innerHTML = '<div class="alert alert-danger" role="alert">Authentication failed. Please check your email and password.</div>';
         }
     } catch (error) {
         console.error('An error occurred:', error);
