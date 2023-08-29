490e6e89d33196f4c79aa9c6c93dd3f9354c200d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 22:25:57 -0600

    update

--- assets/api-scripts/audit_controls.js
+++ assets/api-scripts/audit_controls.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sectionSelector = document.getElementById('section-selector'); // Add an HTML element for section selection
-    const auditControlsList = document.getElementById('audit-controls-list');
+    // Get all accordion items
+    const accordionItems = document.querySelectorAll('.accordion-item');
 
-    // Fetch data from your Flask API when a section is selected
-    sectionSelector.addEventListener('change', () => {
-        const selectedSection = sectionSelector.value;
-
-        // Fetch data for the selected section from the Flask API
-        fetch(`https://app-aarc-api.morganserver.com/api/audit-controls?section=${selectedSection}`)
+    // Function to fetch and display controls for a specific section
+    function displayControls(section) {
+        // Fetch data from your Flask API for the specified section
+        fetch(`https://app-aarc-api.morganserver.com/api/audit-controls?section=${section}`)
             .then((response) => response.json())
             .then((data) => {
+                // Get the HTML element where you want to display the data
+                const controlsList = document.getElementById(`${section.toLowerCase()}-controls`);
+                
                 // Clear the existing content
-                auditControlsList.innerHTML = '';
+                controlsList.innerHTML = '';
 
                 // Process the received data and update the HTML
                 data.forEach((control) => {
@@ -25,11 +26,27 @@ document.addEventListener("DOMContentLoaded", function () {
                     `;
                     
                     // Append the control element to the list
-                    auditControlsList.appendChild(auditControlDiv);
+                    controlsList.appendChild(auditControlDiv);
                 });
             })
             .catch((error) => {
                 console.error('Error:', error);
             });
+    }
+
+    // Add click event listeners to accordion items
+    accordionItems.forEach((item) => {
+        const section = item.getAttribute('data-section');
+        item.querySelector('.accordion-header').addEventListener('click', () => {
+            // Toggle the display of controls when an accordion item is clicked
+            const controlsList = item.querySelector('.accordion-content');
+            if (controlsList.style.display === 'block') {
+                controlsList.style.display = 'none';
+            } else {
+                controlsList.style.display = 'block';
+                // Fetch and display controls for the clicked section
+                displayControls(section);
+            }
+        });
     });
 });
