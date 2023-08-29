5ef739c5d9d74d7a212d2df9166e937d57408257
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 23:11:20 -0600

    update

--- assets/api-scripts/audit_controls.js
+++ assets/api-scripts/audit_controls.js
@@ -3,46 +3,40 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('https://app-aarc-api.morganserver.com/api/audit-controls')
         .then((response) => response.json())
         .then((data) => {
-            // Get the HTML element where you want to display the data for 'CC1'
+            // Get the HTML elements where you want to display the data
             const cc1ControlsList = document.getElementById('cc1-controls-list');
+            const cc2ControlsList = document.getElementById('cc2-controls-list');
 
-            // Check if 'CC1' controls exist
-            if (data['CC1']) {
-                // Process and display 'CC1' controls
-                data['CC1'].forEach((control) => {
-                    // Create a new element for each audit control
-                    const auditControlDiv = document.createElement('div');
-                    auditControlDiv.innerHTML = `
-                        <strong>Scope Category:</strong> ${control.scope_category}<br>
-                        <strong>Control Section:</strong> ${control.control_section}<br>
-                        <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
-                        <strong>Control Activity:</strong> ${control.control_activity}<br><br>
-                    `;
+            // Function to display controls for a given section
+            function displayControls(sectionData, controlsList, sectionName) {
+                if (sectionData) {
+                    // Process and display controls for the section
+                    sectionData.forEach((control) => {
+                        // Create a new element for each audit control
+                        const auditControlDiv = document.createElement('div');
+                        auditControlDiv.innerHTML = `
+                            <strong>Scope Category:</strong> ${control.scope_category}<br>
+                            <strong>Control Section:</strong> ${control.control_section}<br>
+                            <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
+                            <strong>Control Activity:</strong> ${control.control_activity}<br><br>
+                        `;
 
-                    // Append the control element to the 'CC1' controls list
-                    cc1ControlsList.appendChild(auditControlDiv);
-                });
-            } else if (data['CC2']) {
-                // Process and display 'CC1' controls
-                data['CC2'].forEach((control) => {
-                    // Create a new element for each audit control
-                    const auditControlDiv = document.createElement('div');
-                    auditControlDiv.innerHTML = `
-                        <strong>Scope Category:</strong> ${control.scope_category}<br>
-                        <strong>Control Section:</strong> ${control.control_section}<br>
-                        <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
-                        <strong>Control Activity:</strong> ${control.control_activity}<br><br>
-                    `;
-
-                    // Append the control element to the 'CC1' controls list
-                    cc1ControlsList.appendChild(auditControlDiv);
-                });
-            } else {
-                // 'CC1' controls not found
-                const noCC1ControlsDiv = document.createElement('div');
-                noCC1ControlsDiv.textContent = 'No controls found for Section CC1.';
-                cc1ControlsList.appendChild(noCC1ControlsDiv);
+                        // Append the control element to the section's controls list
+                        controlsList.appendChild(auditControlDiv);
+                    });
+                } else {
+                    // Section controls not found
+                    const noControlsDiv = document.createElement('div');
+                    noControlsDiv.textContent = `No controls found for Section ${sectionName}.`;
+                    controlsList.appendChild(noControlsDiv);
+                }
             }
+
+            // Display controls for 'CC1' section
+            displayControls(data['CC1'], cc1ControlsList, 'CC1');
+
+            // Display controls for 'CC2' section
+            displayControls(data['CC2'], cc2ControlsList, 'CC2');
         })
         .catch((error) => {
             console.error('Error:', error);
