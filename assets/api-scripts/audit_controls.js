d78cfaea021405a1fdc04b83bd199974c37304d4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-29 10:29:34 -0600

    update

--- assets/api-scripts/audit_controls.js
+++ assets/api-scripts/audit_controls.js
@@ -13,34 +13,66 @@ document.addEventListener("DOMContentLoaded", function () {
                     // Process and display controls for the section
                     sectionData.forEach((control) => {
                         // Create a new element for each audit control
-                        const auditControlDiv = document.createElement('div');
-                        auditControlDiv.innerHTML = `
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col" style="width: 15%;">Control Key</th> 
-                                            <th scope="col">Point of Focus</th>
-                                            <th scope="col">Illustrative Control</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        ${controls.map(control => `
-                                            <tr>
-                                                <td style="width: 8%;">${control.control_section}</td>
-                                                <td style="width: 45%;">${control.point_of_focus}</td>
-                                                <td style="width: 45%;">${control.control_activity}</td>
-                                            </tr>
-                                        `).join('')}
-                                    </tbody>
-                                </table>
-                            </div>
+                        // const auditControlDiv = document.createElement('div');
+                        // auditControlDiv.innerHTML = `
+                        //     <div class="table-responsive">
+                        //         <table class="table">
+                        //             <thead>
+                        //                 <tr>
+                        //                     <th scope="col" style="width: 15%;">Control Key</th> 
+                        //                     <th scope="col">Point of Focus</th>
+                        //                     <th scope="col">Illustrative Control</th>
+                        //                 </tr>
+                        //             </thead>
+                        //             <tbody>
+                        //                 ${controls.map(control => `
+                        //                     <tr>
+                        //                         <td style="width: 8%;">${control.control_section}</td>
+                        //                         <td style="width: 45%;">${control.point_of_focus}</td>
+                        //                         <td style="width: 45%;">${control.control_activity}</td>
+                        //                     </tr>
+                        //                 `).join('')}
+                        //             </tbody>
+                        //         </table>
+                        //     </div>
+                        // `;
+
+                        // Create the header once
+                        const header = document.createElement('thead');
+                        header.innerHTML = `
+                            <tr>
+                                <th scope="col" style="width: 15%;">Control Key</th> 
+                                <th scope="col">Point of Focus</th>
+                                <th scope="col">Illustrative Control</th>
+                            </tr>
                         `;
+
+                        // Create the table body
+                        const tbody = document.createElement('tbody');
+                        const row = document.createElement('tr');
+                        row.innerHTML = `
+                            <td style="width: 8%;">${control.control_section}</td>
+                            <td style="width: 45%;">${control.point_of_focus}</td>
+                            <td style="width: 45%;">${control.control_activity}</td>
+                        `;
+                        tbody.appendChild(row);
+
+                        // Create the table element and append header and body
+                        const table = document.createElement('table');
+                        table.classList.add('table');
+                        table.appendChild(header);
+                        table.appendChild(tbody);
+
+                        // Create the container div and append the table
+                        const auditControlDiv = document.createElement('div');
+                        auditControlDiv.classList.add('table-responsive');
+                        auditControlDiv.appendChild(table);
+
 
 
 
                         // Append the control element to the section's controls list
-                        controlsList.appendChild(auditControlDiv);
+                        // controlsList.appendChild(auditControlDiv);
                     });
                 } else {
                     // Section controls not found
