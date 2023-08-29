2c8a85ec593938eb15aa9096b0b314b480d32952
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 18:37:15 -0600

    update

---
+++ assets/api-scripts/auth_first.js
@@ -0,0 +1,14 @@
+// Check if an access token exists
+const accessToken = getCookie('access_token');
+
+if (!accessToken) {
+    // Redirect the user to the login page
+    window.location.href = 'https://app-aarc.morganserver.com/';
+}
+
+// Function to retrieve a specific cookie by name
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
