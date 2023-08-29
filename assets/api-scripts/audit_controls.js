a2133bcd778763c53a2a0977e5907d5333d06083
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 23:09:32 -0600

    update

--- assets/api-scripts/audit_controls.js
+++ assets/api-scripts/audit_controls.js
@@ -22,6 +22,21 @@ document.addEventListener("DOMContentLoaded", function () {
                     // Append the control element to the 'CC1' controls list
                     cc1ControlsList.appendChild(auditControlDiv);
                 });
+            } else if (data['CC2']) {
+                // Process and display 'CC1' controls
+                data['CC1'].forEach((control) => {
+                    // Create a new element for each audit control
+                    const auditControlDiv = document.createElement('div');
+                    auditControlDiv.innerHTML = `
+                        <strong>Scope Category:</strong> ${control.scope_category}<br>
+                        <strong>Control Section:</strong> ${control.control_section}<br>
+                        <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
+                        <strong>Control Activity:</strong> ${control.control_activity}<br><br>
+                    `;
+
+                    // Append the control element to the 'CC1' controls list
+                    cc1ControlsList.appendChild(auditControlDiv);
+                });
             } else {
                 // 'CC1' controls not found
                 const noCC1ControlsDiv = document.createElement('div');
