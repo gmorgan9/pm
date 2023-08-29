09b0345f4f5024742d0647082b4a035977265284
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 22:59:50 -0600

    update

--- assets/api-scripts/audit_controls.js
+++ assets/api-scripts/audit_controls.js
@@ -5,52 +5,51 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((data) => {
             // Get the HTML element where you want to display the data
             const auditControlsList = document.getElementById('audit-controls-list');
-            
-            // Create an object to store controls grouped by section_number
-            const sectionControls = {};
+
+            // Create an object to store controls by section_number
+            const controlsBySection = {};
 
             // Process the received data and group controls by section_number
             data.forEach((control) => {
                 const sectionNumber = control.section_number;
 
-                // Create a container for the section if it doesn't exist
-                if (!sectionControls[sectionNumber]) {
-                    sectionControls[sectionNumber] = [];
+                // Initialize the array for the section if it doesn't exist
+                if (!controlsBySection[sectionNumber]) {
+                    controlsBySection[sectionNumber] = [];
                 }
 
-                // Add the control to the respective section container
-                sectionControls[sectionNumber].push(control);
+                // Add the control to the section's array
+                controlsBySection[sectionNumber].push(control);
             });
 
-            // Iterate through sectionControls and create containers for each section
-            for (const sectionNumber in sectionControls) {
-                if (sectionControls.hasOwnProperty(sectionNumber)) {
-                    const sectionContainer = document.createElement('div');
-                    sectionContainer.className = 'section-container'; // You can style this container as needed
-                    
-                    // Create a heading for the section
-                    const sectionHeading = document.createElement('h2');
-                    sectionHeading.textContent = `Section Number: ${sectionNumber}`;
-                    sectionContainer.appendChild(sectionHeading);
+            // Grab controls with section_number 'CC1'
+            const cc1Controls = controlsBySection['CC1'];
 
-                    // Iterate through controls in the section and display them
-                    sectionControls[sectionNumber].forEach((control) => {
-                        // Create a new element for each audit control
-                        const auditControlDiv = document.createElement('div');
-                        auditControlDiv.innerHTML = `
-                            <strong>Scope Category:</strong> ${control.scope_category}<br>
-                            <strong>Control Section:</strong> ${control.control_section}<br>
-                            <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
-                            <strong>Control Activity:</strong> ${control.control_activity}<br><br>
-                        `;
-                        
-                        // Append the control element to the section container
-                        sectionContainer.appendChild(auditControlDiv);
-                    });
+            // Check if there are 'CC1' controls
+            if (cc1Controls) {
+                // Create a new section container for 'CC1'
+                const cc1Container = document.createElement('div');
+                cc1Container.className = 'section-container'; // Add a class for styling
 
-                    // Append the section container to the main auditControlsList
-                    auditControlsList.appendChild(sectionContainer);
-                }
+                // Append the section header for 'CC1'
+                const cc1Header = document.createElement('h2');
+                cc1Header.textContent = 'Section CC1';
+                cc1Container.appendChild(cc1Header);
+
+                // Append each 'CC1' control within the section
+                cc1Controls.forEach((control) => {
+                    const auditControlDiv = document.createElement('div');
+                    auditControlDiv.innerHTML = `
+                        <strong>Scope Category:</strong> ${control.scope_category}<br>
+                        <strong>Control Section:</strong> ${control.control_section}<br>
+                        <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
+                        <strong>Control Activity:</strong> ${control.control_activity}<br><br>
+                    `;
+                    cc1Container.appendChild(auditControlDiv);
+                });
+
+                // Append the 'CC1' section container to the main list
+                auditControlsList.appendChild(cc1Container);
             }
         })
         .catch((error) => {
