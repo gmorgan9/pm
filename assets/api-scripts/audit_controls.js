e9fead950a3f043dc556a28281a02db9515257d3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 22:22:50 -0600

    update

--- assets/api-scripts/audit_controls.js
+++ assets/api-scripts/audit_controls.js
@@ -1,34 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Fetch data from your Flask API
-    fetch('https://app-aarc-api.morganserver.com/api/audit-controls')
-        .then((response) => response.json())
-        .then((data) => {
-            // Filter the data to include only controls with section_number CC1
-            const cc1Controls = data.filter((control) => control.section_number === 'CC1');
-            
-            // Get the HTML element where you want to display the data
-            const auditControlsList = document.getElementById('audit-controls-list');
-            
-            // Process the received data and update the HTML
-            cc1Controls.forEach((control) => {
-                // Create a new element for each audit control
-                const auditControlDiv = document.createElement('div');
-                auditControlDiv.innerHTML = `
-                    <strong>Scope Category:</strong> ${control.scope_category}<br>
-                    <strong>Control Section:</strong> ${control.control_section}<br>
-                    <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
-                    <strong>Control Activity:</strong> ${control.control_activity}<br><br>
-                `;
-                
-                // Append the control element to the list
-                auditControlsList.appendChild(auditControlDiv);
+    const sectionSelector = document.getElementById('section-selector'); // Add an HTML element for section selection
+    const auditControlsList = document.getElementById('audit-controls-list');
+
+    // Fetch data from your Flask API when a section is selected
+    sectionSelector.addEventListener('change', () => {
+        const selectedSection = sectionSelector.value;
+
+        // Fetch data for the selected section from the Flask API
+        fetch(`https://app-aarc-api.morganserver.com/api/audit-controls?section=${selectedSection}`)
+            .then((response) => response.json())
+            .then((data) => {
+                // Clear the existing content
+                auditControlsList.innerHTML = '';
+
+                // Process the received data and update the HTML
+                data.forEach((control) => {
+                    // Create a new element for each audit control
+                    const auditControlDiv = document.createElement('div');
+                    auditControlDiv.innerHTML = `
+                        <strong>Scope Category:</strong> ${control.scope_category}<br>
+                        <strong>Control Section:</strong> ${control.control_section}<br>
+                        <strong>Point of Focus:</strong> ${control.point_of_focus}<br>
+                        <strong>Control Activity:</strong> ${control.control_activity}<br><br>
+                    `;
+                    
+                    // Append the control element to the list
+                    auditControlsList.appendChild(auditControlDiv);
+                });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
             });
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    });
 });
-
-
-// <strong>Section Number:</strong> ${control.section_number}<br>
-// <strong>Control Number:</strong> ${control.control_number}<br>
