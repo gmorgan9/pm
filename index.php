e404164b41dcc7f5965cedcd265f937ad59082be
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-25 23:36:35 -0600

    update

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if(mysqli_num_rows($result) > 0){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - ProjectManager</title>
-    <link rel="stylesheet" href="assets/styles/login-styles.css">
+    <link rel="stylesheet" href="assets/styles/login-styles.css?v=5.1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
