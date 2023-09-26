40b39996113e005cfb4a692c532cdcb1e8be168e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-25 23:30:10 -0600

    update

---
+++ app/connection.php
@@ -0,0 +1,18 @@
+<!-- WORKING -->
+<?php
+
+$servername = "localhost";
+$username = "dbadmin";
+$password = "DBadmin123!";
+$database = "projectmanager";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+} else {
+    print("hello")
+}
+?>
