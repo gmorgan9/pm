2ade246fc26c2484e81c9e30f5f3f21b86fa98f5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-28 12:08:53 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -604,12 +604,8 @@ session_start();
 
 
                                 <p class="text-muted">
-                                   <?php echo $client_name; ?>
-                                </p>
-                                <p class="text-muted">
-                                   <?php echo $f_date; ?>
-                                </p>
-                                <p class="text-muted">
+                                   <?php echo $client_name; ?><br>
+                                   <?php echo $f_date; ?><br>
                                    <?php echo $f_start_time . ' - ' . $f_end_time; ?>
                                 </p>
                              </div>
