e66ef7525177e65e912f19fa4e1231feee90f616
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-28 10:42:18 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -259,7 +259,7 @@ session_start();
                       </table>
 
                       <br>
-                      <div class="d-flex justify-content-center align-bottom">
+                      <div class="mt-auto">
                         <?php
                           // Pagination links
                           $sql = "SELECT COUNT(*) as total FROM engagement";
