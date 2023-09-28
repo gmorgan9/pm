379331685987339afac4d25e69195b7cbd749685
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-28 09:14:35 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -55,7 +55,7 @@ session_start();
 
             <div class="card" style="width: 18rem;">
               <div class="card-body">
-                <p class="card-text">Projects</p>
+                <p class="card-text" style="margin-bottom: -10px;">Projects</p>
                 <h5 class="card-title" style="font-size: 80px;">
                   25
                 </h5>
@@ -66,7 +66,7 @@ session_start();
             <div class="card" style="width: 18rem; ">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                <h6 class="card-subtitle mb-text-body-secondary">Card subtitle</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
