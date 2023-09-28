3f969d0ddb7f19e85176b0c362156e39b3c11758
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-27 19:42:31 -0600

    update

--- add_engagement/index.php
+++ add_engagement/index.php
@@ -256,16 +256,15 @@ if(isset($_POST['add-engagement'])){
           <div class="tab-content" id="nav-tabContent">
             <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
               <div class="row d-flex">
-                <div class="col w-50 align-middle" style="height: 100px;">
+                <div class="col w-50 my-auto d-flex flex-column justify-content-center" style="height: 100px;">
                   <label>Review End Date</label>
                   <input class="form-control" type="date" name="review_end">
                 </div>
                 <div class="col w-50 my-auto d-flex flex-column justify-content-center" style="height: 100px;">
-  <div class="alert alert-primary" role="alert">
-    Only given an "as of date" because of a type 1 engagement.
-  </div>
-</div>
-
+                  <div class="alert alert-primary" role="alert">
+                    Only given an "as of date" because of a type 1 engagement.
+                  </div>
+                </div>
               </div>
             </div>
             <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0" style="height: 100px;">
