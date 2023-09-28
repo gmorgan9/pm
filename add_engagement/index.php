0ad48c028b20cde8e454d39b8fbca115a66a3db3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-27 19:23:31 -0600

    update

--- add_engagement/index.php
+++ add_engagement/index.php
@@ -272,7 +272,7 @@ if(isset($_POST['add-engagement'])){
                   <label>Review End Date</label>
                   <input class="form-control" type="date" name="review_end">
                 </div>
-                <div class="col w-50">
+                <div class="col w-50 d-flex align-items-center">
                   <p>
                     Only given a "as of date" because of a type 1 enagement.
                   </p>
