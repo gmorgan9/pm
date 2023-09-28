2c0e13a86d462386e7c00efa4c961bb7689761b6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-28 11:48:16 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -553,126 +553,127 @@ session_start();
 
 
                   <div class="container">
-   <ul class="timeline">
-      <li>
-         <!-- begin timeline-time -->
-         <div class="timeline-time">
-            <span class="date">today</span>
-            <span class="time">04:20</span>
-         </div>
-         <!-- end timeline-time -->
-         <!-- begin timeline-icon -->
-         <div class="timeline-icon">
-         </div>
-         <!-- end timeline-icon -->
-         <!-- begin timeline-body -->
-         <div class="timeline-body">
-            <div class="timeline-header">
-               <span class="username"><a href="javascript:;">John Smith</a> <small></small></span>
-               <span class="pull-right text-muted">18 Views</span>
-            </div>
-            <div class="timeline-content">
-               <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus turpis quis tincidunt luctus.
-                  Nam sagittis dui in nunc consequat, in imperdiet nunc sagittis.
-               </p>
-            </div>
-            <div class="timeline-likes">
-               <div class="stats-right">
-                  <span class="stats-text">259 Shares</span>
-                  <span class="stats-text">21 Comments</span>
-               </div>
-               <div class="stats">
-                  <span class="fa-stack fa-fw stats-icon">
-                  <i class="fa fa-circle fa-stack-2x text-danger"></i>
-                  <i class="fa fa-heart fa-stack-1x fa-inverse t-plus-1"></i>
-                  </span>
-                  <span class="fa-stack fa-fw stats-icon">
-                  <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                  <i class="fa fa-thumbs-up fa-stack-1x fa-inverse"></i>
-                  </span>
-                  <span class="stats-total">4.3k</span>
-               </div>
-            </div>
-            
-         </div>
-         <!-- end timeline-body -->
-      </li>
-      <li>
-         <!-- begin timeline-time -->
-         <div class="timeline-time">
-            <span class="date">24 February 2014</span>
-            <span class="time">08:17</span>
-         </div>
-         <!-- end timeline-time -->
-         <!-- begin timeline-icon -->
-         <div class="timeline-icon">
-         </div>
-         <!-- end timeline-icon -->
-         <!-- begin timeline-body -->
-         <div class="timeline-body">
-            <div class="timeline-header">
-               <span class="username">Richard Leong</span>
-               <span class="pull-right text-muted">1,282 Views</span>
-            </div>
-            <div class="timeline-content">
-               <p class="lead">
-                  <i class="fa fa-quote-left fa-fw pull-left"></i>
-                  Quisque sed varius nisl. Nulla facilisi. Phasellus consequat sapien sit amet nibh molestie placerat. Donec nulla quam, ullamcorper ut velit vitae, lobortis condimentum magna. Suspendisse mollis in sem vel mollis.
-                  <i class="fa fa-quote-right fa-fw pull-right"></i>
-               </p>
-            </div>
-         </div>
-         <!-- end timeline-body -->
-      </li>
-      <li>
-         <!-- begin timeline-time -->
-         <div class="timeline-time">
-            <span class="date">10 January 2014</span>
-            <span class="time">20:43</span>
-         </div>
-         <!-- end timeline-time -->
-         <!-- begin timeline-icon -->
-         <div class="timeline-icon">
-         </div>
-         <!-- end timeline-icon -->
-         <!-- begin timeline-body -->
-         <div class="timeline-body">
-            <div class="timeline-header">
-               
-               <span class="username">Lelouch Wong</span>
-               <span class="pull-right text-muted">1,021,282 Views</span>
-            </div>
-            <div class="timeline-content">
-               <h4 class="template-title">
-                  <i class="fa fa-map-marker text-danger fa-fw"></i>
-                  795 Folsom Ave, Suite 600 San Francisco, CA 94107
-               </h4>
-               <p>In hac habitasse platea dictumst. Pellentesque bibendum id sem nec faucibus. Maecenas molestie, augue vel accumsan rutrum, massa mi rutrum odio, id luctus mauris nibh ut leo.</p>
-               
-            </div>
-            <div class="timeline-footer">
-               <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-thumbs-up fa-fw fa-lg m-r-3"></i> Like</a>
-               <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-comments fa-fw fa-lg m-r-3"></i> Comment</a> 
-               <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-share fa-fw fa-lg m-r-3"></i> Share</a>
-            </div>
-         </div>
-         <!-- end timeline-body -->
-      </li>
-      <li>
-         <!-- begin timeline-icon -->
-         <div class="timeline-icon">
-            <a href="javascript:;">&nbsp;</a>
-         </div>
-         <!-- end timeline-icon -->
-         <!-- begin timeline-body -->
-         <div class="timeline-body">
-            Loading...
-         </div>
-         <!-- begin timeline-body -->
-      </li>
-   </ul>
-</div>
+                    <ul class="timeline">
+                       <li>
+                          <!-- begin timeline-time -->
+                          <div class="timeline-time">
+                             <span class="date">today</span>
+                             <span class="time">04:20</span>
+                          </div>
+                          <!-- end timeline-time -->
+                          <!-- begin timeline-icon -->
+                          <div class="timeline-icon">
+                          </div>
+                          <!-- end timeline-icon -->
+                          <!-- begin timeline-body -->
+                          <div class="timeline-body">
+                             <div class="timeline-header">
+                                <span class="username"><a href="javascript:;">John Smith</a> <small></small></span>
+                                <span class="pull-right text-muted">18 Views</span>
+                             </div>
+                             <div class="timeline-content">
+                                <p>
+                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus turpis quis tincidunt luctus.
+                                   Nam sagittis dui in nunc consequat, in imperdiet nunc sagittis.
+                                </p>
+                             </div>
+                             <div class="timeline-likes">
+                                <div class="stats-right">
+                                   <span class="stats-text">259 Shares</span>
+                                   <span class="stats-text">21 Comments</span>
+                                </div>
+                                <div class="stats">
+                                   <span class="fa-stack fa-fw stats-icon">
+                                   <i class="fa fa-circle fa-stack-2x text-danger"></i>
+                                   <i class="fa fa-heart fa-stack-1x fa-inverse t-plus-1"></i>
+                                   </span>
+                                   <span class="fa-stack fa-fw stats-icon">
+                                   <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                                   <i class="fa fa-thumbs-up fa-stack-1x fa-inverse"></i>
+                                   </span>
+                                   <span class="stats-total">4.3k</span>
+                                </div>
+                             </div>
+
+                          </div>
+                          <!-- end timeline-body -->
+                       </li>
+                       <li>
+                          <!-- begin timeline-time -->
+                          <div class="timeline-time">
+                             <span class="date">24 February 2014</span>
+                             <span class="time">08:17</span>
+                          </div>
+                          <!-- end timeline-time -->
+                          <!-- begin timeline-icon -->
+                          <div class="timeline-icon">
+                          </div>
+                          <!-- end timeline-icon -->
+                          <!-- begin timeline-body -->
+                          <div class="timeline-body">
+                             <div class="timeline-header">
+                                <span class="username">Richard Leong</span>
+                                <span class="pull-right text-muted">1,282 Views</span>
+                             </div>
+                             <div class="timeline-content">
+                                <p class="lead">
+                                   <i class="fa fa-quote-left fa-fw pull-left"></i>
+                                   Quisque sed varius nisl. Nulla facilisi. Phasellus consequat sapien sit amet nibh molestie placerat. Donec nulla quam, ullamcorper ut velit vitae, lobortis condimentum magna. Suspendisse mollis in sem vel mollis.
+                                   <i class="fa fa-quote-right fa-fw pull-right"></i>
+                                </p>
+                             </div>
+                          </div>
+                          <!-- end timeline-body -->
+                       </li>
+                       <li>
+                          <!-- begin timeline-time -->
+                          <div class="timeline-time">
+                             <span class="date">10 January 2014</span>
+                             <span class="time">20:43</span>
+                          </div>
+                          <!-- end timeline-time -->
+                          <!-- begin timeline-icon -->
+                          <div class="timeline-icon">
+                          <a href="javascript:;">&nbsp;</a>
+                          </div>
+                          <!-- end timeline-icon -->
+                          <!-- begin timeline-body -->
+                          <div class="timeline-body">
+                             <div class="timeline-header">
+
+                                <span class="username">Lelouch Wong</span>
+                                <span class="pull-right text-muted">1,021,282 Views</span>
+                             </div>
+                             <div class="timeline-content">
+                                <h4 class="template-title">
+                                   <i class="fa fa-map-marker text-danger fa-fw"></i>
+                                   795 Folsom Ave, Suite 600 San Francisco, CA 94107
+                                </h4>
+                                <p>In hac habitasse platea dictumst. Pellentesque bibendum id sem nec faucibus. Maecenas molestie, augue vel accumsan rutrum, massa mi rutrum odio, id luctus mauris nibh ut leo.</p>
+
+                             </div>
+                             <div class="timeline-footer">
+                                <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-thumbs-up fa-fw fa-lg m-r-3"></i> Like</a>
+                                <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-comments fa-fw fa-lg m-r-3"></i> Comment</a> 
+                                <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-share fa-fw fa-lg m-r-3"></i> Share</a>
+                             </div>
+                          </div>
+                          <!-- end timeline-body -->
+                       </li>
+                       <li>
+                          <!-- begin timeline-icon -->
+                          <div class="timeline-icon">
+                             <a href="javascript:;">&nbsp;</a>
+                          </div>
+                          <!-- end timeline-icon -->
+                          <!-- begin timeline-body -->
+                          <div class="timeline-body">
+                             Loading...
+                          </div>
+                          <!-- begin timeline-body -->
+                       </li>
+                    </ul>
+                  </div>
 
 
 
