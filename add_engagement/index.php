5066ffa3cc84bb2ce286e098d030d259484429c6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-27 19:21:44 -0600

    update

--- add_engagement/index.php
+++ add_engagement/index.php
@@ -256,13 +256,15 @@ if(isset($_POST['add-engagement'])){
           <div class="tab-content" id="nav-tabContent">
              <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
 
-                <div class="col w-50">
-                  <label>Review Start Date</label>
-                  <input class="form-control" type="date" name="review_start">
-                </div>          
-                <div class="col w-50">
-                  <label>Review End Date</label>
-                  <input class="form-control" type="date" name="review_end">
+                <div class="row d-flex">
+                  <div class="col w-50">
+                    <label>Review Start Date</label>
+                    <input class="form-control" type="date" name="review_start">
+                  </div>          
+                  <div class="col w-50">
+                    <label>Review End Date</label>
+                    <input class="form-control" type="date" name="review_end">
+                  </div>
                 </div>
 
               </div>
