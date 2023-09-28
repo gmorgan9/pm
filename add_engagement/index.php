b683f11f09e4489cb10de126f0dab4f1d0e933fe
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-27 20:01:21 -0600

    update

--- add_engagement/index.php
+++ add_engagement/index.php
@@ -246,7 +246,7 @@ if(isset($_POST['add-engagement'])){
 
         <!-- Start staff row -->
         <div class="row d-flex">
-          <div class="soc2" style="border: 1px solid black;">
+          <div class="soc2 d-flex" style="border: 1px solid black;">
             <div class="col w-50">
               <label>Review Start Date</label>
               <input class="form-control" type="date" name="review_start">
