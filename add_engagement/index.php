cac144cea3758e7f7f74965f64f9e5740b5d6923
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-27 21:06:48 -0600

    update

--- add_engagement/index.php
+++ add_engagement/index.php
@@ -293,7 +293,7 @@ if(isset($_POST['add-engagement'])){
             </div>
           </div>
 
-          <div class="row d-flex" style="max-width: 100%;">
+          <div class="row d-flex flex-wrap">
             <div class="col w-25">
               <label>Fieldwork Documentation Date</label>
               <input class="form-control" type="date" name="review_start">
