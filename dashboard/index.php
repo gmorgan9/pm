bb6b22b85cdca8531737972f03389565a5c70a38
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-09-28 19:47:01 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -641,7 +641,7 @@ session_start();
 
                           <!-- VIEW Modal -->
                             <div class="modal fade" id="viewModal<?php echo $id; ?>" tabindex="-1" aria-labelledby="viewModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-lg">
+                                <div class="modal-dialog modal-lg modal-dialog-scrollable">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="viewModalLabel">View Engagement</h5>
